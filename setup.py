638fc391363f89c2f89b6b53a20c5fb7e9d138fe
Peter Macgregor <macgregor.pr@gmail.com>
2022-10-05 13:59:16 +0100

    Configure setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup, find_packages
+from distutils.core import setup, Extension
 
 VERSION = '0.1'
 DESCRIPTION = 'STAG - Spectral Algorithms for Graphs'
 LONG_DESCRIPTION =\
     "This library provides several methods and algorithms relating to spectral graph theory in python."
 
+# specify the name of the extension and source files
+# required to compile this
+# ext_modules = [Extension(name='swig.stag_internal',
+#                          sources=["swig/stag_internal.i"],
+#                          swig_opts=["-c++"],
+#                          # libraries=['Eigen'],
+#                          # library_dirs=["eigen-3.3.9"],
+#                          include_dirs=["eigen-3.3.9", "stag_lib"])]
+
+ext_modules = [Extension(name='swig._stag_internal',
+                         sources=["swig/stag_internal_wrap.cxx", "stag_lib/stag.cpp", "stag_lib/utility.cpp"],
+                         include_dirs=["eigen-3.3.9", "stag_lib"])]
+
 # Setting up
 setup(
     name="stag",
+    ext_modules=ext_modules,
     version=VERSION,
     author="Peter Macgregor",
     author_email="<macgregor.pr@gmail.com>",

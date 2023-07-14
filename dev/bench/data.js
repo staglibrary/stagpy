e01f3b913d3bd5df7ee329b8cfe88c93797e8488
github-action-benchmark <github@users.noreply.github.com>
2023-07-14 13:31:37 +0000

    add Performance Tests (pytest) benchmark result for 022588c750eeba946024e1e9dcaa7e6271c112d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689341317073,
+  "lastUpdate": 1689341497939,
   "repoUrl": "https://github.com/staglibrary/stagpy",
   "entries": {
     "Performance Tests": [
@@ -107,6 +107,70 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00319672977905899",
             "extra": "mean: 40.57258172726883 msec\nrounds: 22"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.macgregor@ed.ac.uk",
+            "name": "Peter Macgregor"
+          },
+          "committer": {
+            "email": "peter.macgregor@ed.ac.uk",
+            "name": "Peter Macgregor"
+          },
+          "distinct": true,
+          "id": "022588c750eeba946024e1e9dcaa7e6271c112d1",
+          "message": "Add compute eigensystem perf test",
+          "timestamp": "2023-07-14T14:29:54+01:00",
+          "tree_id": "54c4a2ff2a1fc0b79fefa1385ab4d4d5d6fde0f1",
+          "url": "https://github.com/staglibrary/stagpy/commit/022588c750eeba946024e1e9dcaa7e6271c112d1"
+        },
+        "date": 1689341496672,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "test/test_performance.py::test_sbm",
+            "value": 554.8879692793779,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000023404073961226265",
+            "extra": "mean: 1.802165581817678 msec\nrounds: 495"
+          },
+          {
+            "name": "test/test_performance.py::test_load_edgelist",
+            "value": 302.17371137301876,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014740865807531509",
+            "extra": "mean: 3.309354726644465 msec\nrounds: 289"
+          },
+          {
+            "name": "test/test_performance.py::test_spectral_cluster",
+            "value": 29.490034385812674,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00024071302466265824",
+            "extra": "mean: 33.90976039285626 msec\nrounds: 28"
+          },
+          {
+            "name": "test/test_performance.py::test_local_cluster",
+            "value": 4191.108061886573,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000023765544670311305",
+            "extra": "mean: 238.60038568174335 usec\nrounds: 2598"
+          },
+          {
+            "name": "test/test_performance.py::test_compute_eigensystem",
+            "value": 37.51512521577956,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0021629478230264293",
+            "extra": "mean: 26.65591529411666 msec\nrounds: 34"
+          },
+          {
+            "name": "test/test_performance.py::test_lap_eigvecs",
+            "value": 29.43502810318306,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002263233308958793",
+            "extra": "mean: 33.97312876667039 msec\nrounds: 30"
           }
         ]
       }

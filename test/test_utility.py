10ba1db4aa0d3893385b66b41793d3b85d512840
Peter Macgregor <peter.macgregor@ed.ac.uk>
2023-07-31 11:30:59 +0100

    Fix utility tests

--- test/test_utility.py
+++ test/test_utility.py
@@ -12,10 +12,6 @@ import stag.utility
 
 def test_numpy_data_types():
     np_vec = np.asarray([1, 2, 3], dtype=np.int64)
-
-    # This should not throw an error.
-    stag_vec = stag.stag_internal.vectorl(np_vec)
-
     np_vec_2 = np.asarray([2, 3, 4], dtype=np.int64)
     sym_diff = stag.cluster.symmetric_difference(np_vec, np_vec_2)
     assert set(sym_diff) == {1, 4}

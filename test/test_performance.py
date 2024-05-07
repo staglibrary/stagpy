74b335edf5fb39f46003115f0d48c6758fd7dabd
Peter Macgregor <peter.macgregor@ed.ac.uk>
2024-05-07 10:27:07 +0100

    Add new benchmark tests for kde stuff

--- test/test_performance.py
+++ test/test_performance.py
@@ -9,6 +9,8 @@ import stag.random
 import stag.graphio
 import stag.cluster
 import stag.spectrum
+import stag.data
+import stag.kde
 
 def test_sbm(benchmark):
     benchmark(stag.random.sbm, 1000, 10, 0.1, 0.01)
@@ -27,6 +29,22 @@ def test_local_cluster(benchmark):
 def test_compute_eigensystem(benchmark):
     g = stag.graphio.load_edgelist("data/test6.edgelist")
     benchmark(stag.spectrum.compute_eigensystem, g, "Laplacian", 10, "Smallest")
+
+def test_construct_ckns_kde(benchmark):
+    data = stag.data.load_matrix("data/mnist.txt")
+    a = 0.000001
+    benchmark(stag.kde.CKNSGaussianKDE, data, a)
+
+def test_query_ckns_kde(benchmark):
+    data = stag.data.load_matrix("data/mnist.txt")
+    a = 0.000001
+    ckns_kde = stag.kde.CKNSGaussianKDE(data, a)
+    benchmark(ckns_kde.query, data)
+
+def test_approximate_similarity_graph(benchmark):
+    data = stag.data.load_matrix("data/mnist.txt")
+    a = 0.000001
+    benchmark(stag.cluster.approximate_similarity_graph, data, a)
 
 #-------------------------------------------------------------------------------
 # Multi-step workflows - this is to test the efficiency of passing data back and
